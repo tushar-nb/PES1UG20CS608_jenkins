0aea7cb7d6b491d4e849f1ff91c383f6a080919f
tushar-nb <tushar02borkade@gmail.com>
2023-02-14 21:25:41 +0530

    second by pes1ug20cs608

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout<<"My SRN PES1UG20CS608";
+  cout<<"My SRN PES1UG20CS608 and NAME is Tushar N Borkade \n";
   cout << "I have successfully built and run\n";
   return 0;
 }
